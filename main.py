aee2b98d1b15794e682cf6e6853f8058f6ac3122
LeMadHatter <sirehatter@gmail.com>
2024-09-04 12:42:12 -0400

    Ship moves and zoooooooooooooooooooooooooooms

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
     print("Starting asteroids!")
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
-        screen.fill("#000000")
         pygame.display.flip()
+        screen.fill("#000000")
+
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
