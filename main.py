6e6d58b0d036ae5718bcbe1af16d75706ddabcc8
LeMadHatter <sirehatter@gmail.com>
2024-09-05 19:10:57 -0400

    Converting into exe

--- main.py
+++ main.py
@@ -49,6 +49,16 @@ def main():
             
         dt = game_clock.tick(60) / 1000
 
+    #https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+    def resource_path(relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        try:
+            # PyInstaller creates a temp folder and stores path in _MEIPASS
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+
+        return os.path.join(base_path, relative_path)
 
 if __name__ == "__main__":
     main()
