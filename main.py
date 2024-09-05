74f60abf24fef1310e349c95ed8280dbd774294f
LeMadHatter <sirehatter@gmail.com>
2024-09-05 16:30:50 -0400

    Ship stays in the screen (very helpful) and shoots but shots can't destroy the asteroids (not very gameful)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from player import *
 from constants import *
+from asteroids import *
+from asteroidfield import *
+from shot import *
+import sys
 
 def main():
     print("Starting asteroids!")
@@ -12,21 +16,33 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
     while True:
         pygame.display.flip()
         screen.fill("#000000")
-
+        
         for item in updatable:
             item.update(dt)
+        for asteroid in asteroids:
+            if asteroid.is_colliding(player) and not player.invicibility:
+                sys.exit("GAME OVER NOOB!")
         for item in drawable:
             item.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         dt = game_clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -1,10 +1,14 @@
 from circleshape import *
 from constants import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.rotation = 0
+        self.shot_timer = 0
+        self.key_timer = 0
+        self.invicibility = True
 
     # in the player class
     def triangle(self):
@@ -20,10 +24,11 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation = self.rotation % 360
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -32,9 +37,48 @@ class Player(CircleShape):
             self.move(dt, keys[pygame.K_LCTRL])
         if keys[pygame.K_s]:
             self.move(-dt, keys[pygame.K_LCTRL])
+        if keys[pygame.K_r]:
+            self.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        if keys[pygame.K_i]:
+            if self.key_timer > 0:
+                return
+            self.invicibility = not self.invicibility
+            print(f"Invicible : {self.invicibility}")
+            self.key_timer = KEY_COOLDOWN
+
+        self.key_timer -= dt
+        self.shot_timer -= dt
 
     def move(self, dt, sprint):
         if sprint:
             dt *= 5
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
+
+        if (self.position.x + forward.x * PLAYER_SPEED * dt > SCREEN_WIDTH 
+            or self.position.x + forward.x * PLAYER_SPEED * dt < 0):
+            forward = pygame.Vector2(0, pygame.Vector2(0, 1).rotate(self.rotation).y)
+
+        if (self.position.y + forward.y * PLAYER_SPEED * dt > SCREEN_HEIGHT
+            or self.position.y + forward.y * PLAYER_SPEED * dt < 0):
+            forward = pygame.Vector2(pygame.Vector2(0,1).rotate(self.rotation).x, 0)      
+
+        if (self.position.x + forward.x * PLAYER_SPEED * dt > SCREEN_WIDTH 
+            or self.position.x + forward.x * PLAYER_SPEED * dt < 0):
+            forward.x = 0
+
+        if (self.position.y + forward.y * PLAYER_SPEED * dt > SCREEN_HEIGHT
+            or self.position.y + forward.y * PLAYER_SPEED * dt < 0):
+            forward.y = 0
+
         self.position += forward * PLAYER_SPEED * dt
+    
+
+    def shoot(self):
+        if self.shot_timer > 0:
+            return
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
+        new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
